d581d5d1b58dab5018e6b195605ee8c11d746745
zhlingzi <32724455+zhlingzi@users.noreply.github.com>
2017-10-13 18:36:44 +0800

    输出测试报告
    执行测试用例并输出测试报告

---
+++ run_main.py
@@ -0,0 +1,35 @@
+# -*- coding:utf8 -*-
+''' Create on "2017/10/13 16:23"
+    _author_="zhao ling"
+    Poject: 输出测试报告
+    只能在命令行下 python执行
+    ide找不到HTMLTestRunner包
+    添加路径也不行的
+ '''
+import testcase
+import unittest
+import time,sys
+from HTMLTestRunner import HTMLTestRunner
+
+
+if __name__ == '__main__':
+
+    suites = unittest.TestSuite()
+
+    suites.addTest(testcase.testnumber('test_add'))
+    suites.addTest(testcase.testnumber('test_noNone'))
+
+    #runner = unittest.TextTestRunner()
+    #repo = runner.run(suites)
+
+    # set report
+    now = time.strftime('%Y-%m-%d %T_%M_%S')
+    #filename = './report/'+ now + '_result.html'
+    filename =' result.html'
+    fp = open(filename,'wb')
+
+    repohtml = HTMLTestRunner(stream = fp,title = 'Number test report',
+                              description = 'Implementation:')
+    repohtml.run(suites)
+    fp.close()
+
